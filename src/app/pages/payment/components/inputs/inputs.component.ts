cbb2f7a02356323207be8abbc7e9ae773784203b
Moses Ebenezer <moseschimdike@gmail.com>
2021-02-17 22:28:20 +0100

    forms

--- src/app/pages/payment/components/inputs/inputs.component.ts
+++ src/app/pages/payment/components/inputs/inputs.component.ts
@@ -13,8 +13,9 @@ export class InputsComponent implements OnInit {
   cardDetailsForm: FormGroup;
 
   onSubmit() {
-    // TODO: Use EventEmitter with form value
     console.warn(this.cardDetailsForm.value);
+    this.cardDetailsForm.reset()
+
   }
 
   ngOnInit(): void {
@@ -22,11 +23,31 @@ export class InputsComponent implements OnInit {
 
       'cardHolder': new FormControl(null, [Validators.required]),
       'cardNumber': new FormControl(null, [Validators.required]),
-      'expirationDate': new FormControl(null, [Validators.required, Validators.pattern('for regex string here')]), // (mandatory, Date, >CurrentDate)
-      'securityCode': new FormControl(null, [Validators.minLength(3), Validators.maxLength(3)]),
-      'amount': new FormControl(null, [Validators.required, Validators.min(0)]),
-  
+      'expirationDate': new FormControl(null, [Validators.required, // Validators.pattern('')
+      ]), // (mandatory, Date, >CurrentDate)
+      'securityCode': new FormControl(null, [Validators.minLength(3), Validators.maxLength(3), Validators.pattern('[0-9]+')]),
+      'amount': new FormControl(null, [Validators.required, Validators.min(0), Validators.pattern('^[1-9][0-9]*$')]),
     })
+  }
+
+  get cardHolder() {
+    return this.cardDetailsForm.get('cardHolder')
+  }
+
+  get cardNumber() {
+    return this.cardDetailsForm.get('cardNumber')
+  }
+
+  get amount() {
+    return this.cardDetailsForm.get('amount')
+  }
+
+  get expirationDate() {
+    return this.cardDetailsForm.get('expirationDate')
+  }
+
+  get securityCode() {
+    return this.cardDetailsForm.get('securityCode')
   }
 
 }
